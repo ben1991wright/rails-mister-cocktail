023f71b7da02aadbc24063718fc0b42abd6e9e99
Ben Wright <ben1991wright@hotmail.co.uk>
2018-02-22 11:47:11 +0000

    Made models and seeded ingredient db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+list = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+
+list['drinks'].each { |hash| Ingredient.create(name: hash['strIngredient1']) }
